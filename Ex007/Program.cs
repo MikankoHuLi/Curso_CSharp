ef7c6d2b1301e774273af28d8887fb8109f4695e
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-16 21:06:51 -0300

    Atualização do exercício

---
+++ Ex007/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final = 70;
+            string resultado;
+            if (nota_final >= 60)
+            {
+                resultado = "aprovado";
+            }
+            else
+            {
+                resultado = "reprovado";
+            }
+            Console.WriteLine("O aluno foi {0}.", resultado); 
+        }
+    }
+}
