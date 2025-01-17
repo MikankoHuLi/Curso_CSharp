f46bd96676d3e8184ee5a422f367869481cd5a1f
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-16 22:24:48 -0300

    pares e ímpares

---
+++ Ex008/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string resultado;
+
+                Console.Write("Digite um número:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 ==0)
+            {
+                resultado = "Par";
+            }
+            else
+            {
+                resultado = "Ímpar";
+            }
+            Console.Write("Ele é {0}", resultado);
+        }
+    }
+}
