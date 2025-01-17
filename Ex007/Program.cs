dc6dc7580917b81a717c67130bc5ec5c761951a1
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-16 21:22:57 -0300

    adicionado recuperação

--- Ex007/Program.cs
+++ Ex007/Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 70;
+            double nota_final = 30;
             string resultado;
             if (nota_final >= 60)
             {
                 resultado = "aprovado";
+            }
+            else if (nota_final >= 40)
+            {
+                resultado = "recuperação";   
             }
             else
             {
