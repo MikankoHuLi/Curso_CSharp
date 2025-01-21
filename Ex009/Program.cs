b1da3271eae4a811af16ce7fca9320d074f9fbe4
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-21 19:32:02 -0300

    Triangulos

---
+++ Ex009/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoC;
+
+            Console.Write("Digite o valor do A:");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do B:");
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do C:");
+            ladoC = Convert.ToInt32(Console.ReadLine());
+
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoA + ladoC))
+            {
+                Console.WriteLine("Os valores informados formam um triângulo");
+            }
+            else
+            {
+                Console.WriteLine("Os valores informado não formam um triângulo");
+            }
+            
+                
+             
+
+        }
+    }
+}
