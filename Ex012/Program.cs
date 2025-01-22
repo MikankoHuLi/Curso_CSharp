8ab19b09afcc4708d9dfcfbd10b78f5f2ccc3a35
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-21 21:48:04 -0300

    numeros maiores

---
+++ Ex012/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Ex012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+
+            Console.Write("Insira um número: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insira um segundo número: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("O primeiro número é maior");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("O segundo número é maior");
+            }
+            else
+            {
+                Console.WriteLine("Não existe valor maior, os dois são iguais");
+            }
+
+        }
+    }
+}
