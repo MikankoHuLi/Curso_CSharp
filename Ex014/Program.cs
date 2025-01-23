7ebb8968529691b76e5c8d1475f667322169ba85
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-23 18:33:21 -0300

    natacao

---
+++ Ex014/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ex014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nasc, atual;
+
+            Console.Write("Digite o ano do seu nascimento: ");
+            nasc = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o ano atual: ");
+            atual = Convert.ToInt32(Console.ReadLine());
+
+            if (atual - nasc < 9)
+            {
+                Console.Write("Você pertence a categoria Mirim");
+            }
+            else if (atual - nasc < 14)
+            {
+                Console.Write("Você pertence a categoria Infantil");
+            }
+            else if (atual - nasc < 19)
+            {
+                Console.Write("Você pertence a categoria Júnior");
+            }
+            else if (atual - nasc < 25)
+            {
+                Console.Write("Você pertence a categoria Sênior");
+            }
+            else
+            {
+                Console.Write("Você pertence a categoria Master");
+            }
+        }
+    }
+}
