80fe8ab2830f49f6e8b33d120d666e1b7f85183a
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-23 20:56:34 -0300

    tabuada melhorada

---
+++ Ex017/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int user;
+            Console.Write("Digite um número: ");
+           user = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} X {1} = {2}", user, i, user * i);
+            }
+        }
+    }
+}
