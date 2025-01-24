c4120264e2062cf47a2464deb5ec42af99cf278e
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-23 21:29:21 -0300

    boas vindas cursos

---
+++ Ex018/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string gen;
+            Console.Write("Digite o gênero: ");
+            gen = Console.ReadLine();
+
+            while (gen != "F" && gen != "M" && gen != "NB") 
+            {
+                Console.Write("Digite o gênero: ");
+                gen = Console.ReadLine();
+            }
+            Console.Write("Bem-Vindes ao curso de C#");
+        }
+    }
+}
