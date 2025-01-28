a05d2ffce17efb68cdaeea1e745a8b107f0aed1e
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-28 20:17:22 -0300

    formas de int

---
+++ Ex022/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+
+            num[0] = 10;
+            num[1] = 20;
+            num[2] = 30;
+            num[3] = 40;
+            num[4] = 50;
+            //num[5] = 60; erro, passou dos limites do vetor (ah é)
+
+            Console.WriteLine(num[4]);
+
+            int[] n = new int[5] { 60, 70, 80, 90, 100 };
+
+            Console.WriteLine(n[4]);
+
+            int[] numero = { 65, 25, 84, 74, 12, 8 };
+
+            Console.WriteLine(numero[4]);
+
+
+        }
+    }
+}
