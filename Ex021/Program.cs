c5e7532b4a33ae71d4432b39884b146960a8881a
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-28 19:42:33 -0300

    tentativas senha

--- Ex021/Program.cs
+++ Ex021/Program.cs
@@ -4,16 +4,35 @@
     {
         static void Main(string[] args)
         {
-            string senha = "123";
-            string senhadig;
+            string senha = "123"; // senha correta
+            string senhadig; // senha digitada pelo user
+            int tentativas = 0; // contador tentativas
 
-            do
+            // loop para solicitar senha usuário
+            do 
             {
+                Console.Clear();
                 Console.Write("Digite a senha: ");
                 senhadig = Console.ReadLine();
+                tentativas ++; //incrementa contador tentativas
+
+                if (senhadig != senha && tentativas < 3)
+                {
+                    Console.WriteLine("Senha Incorreta");
+                    Console.WriteLine("Você tem ainda tem " + (3 - tentativas) + " tentativa(s)");
+                    Console.ReadKey(); 
+                }
+
+                else if (senhadig != senha && tentativas == 3)
+                {
+                    Console.WriteLine("Senha Incorreta - Seu acesso está bloqueado");
+                    return;
+                }
+
             } while (senhadig != senha);
 
-            Console.Write("Senha Correta");
+            Console.Clear();
+            Console.Write("Senha Correta - Tentativas: " + tentativas);
 
 
         }
