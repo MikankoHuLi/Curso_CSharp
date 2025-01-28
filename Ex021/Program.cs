e8eb58f5b04304f6e6f2cf3d6faf8d83b3546f9a
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-28 19:03:26 -0300

    senha

---
+++ Ex021/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123";
+            string senhadig;
+
+            do
+            {
+                Console.Write("Digite a senha: ");
+                senhadig = Console.ReadLine();
+            } while (senhadig != senha);
+
+            Console.Write("Senha Correta");
+
+
+        }
+    }
+}
