16208c5f7c70f3d32e08218ad695b79bd7e876bd
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-28 21:53:14 -0300

    datas

---
+++ Ex023/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meses = { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", 
+                "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
+
+            int[] dias = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            for (int i = 0; i < meses.Length; i++)
+            {
+                Console.WriteLine("O mês de " + meses[i] + " tem " + dias[i] + " dias");    
+            }
+        }
+    }
+}
