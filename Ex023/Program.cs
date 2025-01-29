614b2ae9fa9ae6190d480a6fe76d579b8fdb1b72
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-28 22:13:25 -0300

    num.comandos

--- Ex023/Program.cs
+++ Ex023/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            /*
             string[] meses = { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", 
                 "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
 
@@ -13,6 +14,28 @@
             {
                 Console.WriteLine("O mês de " + meses[i] + " tem " + dias[i] + " dias");    
             }
+
+            string[] semana = { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
+
+            foreach ( string dia in semana)
+            {
+                Console.WriteLine(dia);
+            }
+            */
+
+            int[] num = { 10, 45, 22, 50, 6, -84, -2, 91, -23 };
+            
+
+            Console.WriteLine(num.First());
+            Console.WriteLine(num.Last());
+            Console.WriteLine();
+            Console.WriteLine(num.Max());
+            Console.WriteLine(num.Min());
+            Console.WriteLine();
+            Console.WriteLine(num.Average());
+           
+
+
         }
     }
 }
