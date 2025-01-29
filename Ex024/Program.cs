fe768fcb3c1649da2597132ab677c355d50e9461
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-28 22:26:12 -0300

    frutas

---
+++ Ex024/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frutas = { "banana", "melancia", "pitaya", "kiwi", "carambola",
+                "fruta antiga", "morango", "mirtilo", "lichia", "pêssego" };
+            double[] precos = { 10, 38.40, 88.50, 7, 56.20, 33, 42, 11.99, 9.99, 10.50 };
+
+            for (int i = 0; i < frutas.Length; i++)
+            {
+                Console.WriteLine("O preço da " + frutas[i] + " é " + precos[i]);
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("O maior preço é " + precos.Max());
+            Console.WriteLine("O menor preço é " + precos.Min());
+            Console.WriteLine("A média dos preços é " + precos.Average());
+
+        }
+    }
+}
