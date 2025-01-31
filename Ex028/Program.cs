658703e08b0dfaaa6a20b8434c58a65ccb90bc01
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-30 21:28:38 -0300

    area triangulo

---
+++ Ex028/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex028
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Área do Triângulo");
+            Console.WriteLine("-----------------");
+
+            Console.WriteLine("Digite o tamanho da base do triângulo");
+            double Base = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a altura do triângulo");
+            double alt = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Area(Base, alt);
+        }
+
+        static void Area(double Base, double alt)
+        {
+            double area = (Base * alt) / 2;
+            Console.WriteLine($"A área do triângulo de base de {Base} e altura de {alt} é igual {area}. ");
+        }
+    }
+}
