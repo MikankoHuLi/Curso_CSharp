b552b497906cecefbc9c5ae42576bea7f99f4ad3
Dinizo <guilherme.dcorrea1@senacsp.edu.br>
2025-01-30 21:49:18 -0300

    texto e cabecario

--- Ex027/Program.cs
+++ Ex027/Program.cs
@@ -1,8 +1,17 @@
-﻿namespace Ex027
+﻿using System;
+
+namespace Ex027
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+            
+            AreaTerreno(largura, comprimento);
+
+        }
+
+        static void AreaTerreno(double l, double c)
         {
             Console.WriteLine("====================");
             Console.WriteLine("Controle de Terrenos");
@@ -16,12 +25,6 @@
             Console.Write("Digite o comprimento do tereno em metros: ");
             double comprimento = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine();
-            AreaTerreno(largura, comprimento);
-
-        }
-
-        static void AreaTerreno(double l, double c)
-        {
             double a = l * c;
             Console.WriteLine($"A área do terrno de {l}m x {c}m é de {a:F2}m².");
         }
---
+++ Ex029/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex029
+{
+    internal class Program
+    {
+        static void Escrever(string texto)
+        {
+            int tam = texto.Length + 4;
+            string linha = new ('=', tam);
+            string textocent = texto.PadLeft(tam - 2).PadRight(tam - 2);
+            Console.WriteLine(linha);
+            Console.WriteLine(textocent);
+            Console.WriteLine(linha);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Escrever("Guilherme Diniz Correa");
+            Console.WriteLine();
+        }
+    }
+}
